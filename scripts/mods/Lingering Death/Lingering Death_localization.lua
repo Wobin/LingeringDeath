85aaef7b44926b4e0b38b1ecb6bdaadd5b2b57c5
deluxghost <deluxghost@users.noreply.github.com>
2023-03-06 17:14:36 +0800

    add zh-cn localization

--- scripts/mods/Lingering Death/Lingering Death_localization.lua
+++ scripts/mods/Lingering Death/Lingering Death_localization.lua
@@ -1,11 +1,14 @@
 return {	
-  	mod_description = {
-		en = "Adjusts the delay in the combat feed before entries disappear",
-	},
+  mod_description = {
+    en = "Adjusts the delay in the combat feed before entries disappear",
+    ["zh-cn"] = "调整击杀信息显示时长",
+  },
   combat_list_delay = {
-    en = "Delay in seconds before fade out"
+    en = "Delay in seconds before fade out",
+    ["zh-cn"] = "击杀消息消失前显示的秒数",
   },
   combat_list_notification = {
-    en = "Show delay value on mission load"
-    }
+    en = "Show delay value on mission load",
+    ["zh-cn"] = "任务加载时显示击杀消息时长",
+  }
 }
