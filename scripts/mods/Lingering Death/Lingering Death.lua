c222315336f232fbe65da298775bbd00ba52816d
Ben <Lai>
2024-02-25 21:04:45 +1100

    Fix for null check

--- scripts/mods/Lingering Death/Lingering Death.lua
+++ scripts/mods/Lingering Death/Lingering Death.lua
@@ -1,14 +1,16 @@
 --[[
 Title: Lingering Death
 Author: Wobin
-Date: 27/02/2023
-Version: 1.2
+Date: 23/02/2024
+Version: 1.3
 Repository: https://github.com/Wobin/LingeringDeath
 ]]--
 local mod = get_mod("Lingering Death")
 
 mod:hook(CLASS.HudElementCombatFeed, "_create_notification_entry", function(func, self, notification_type)    
     local notif = func(self, notification_type)
-    notif.total_time = mod:get("combat_list_delay") or 5
+    if notif then
+      notif.total_time = mod:get("combat_list_delay") or 5
+    end
     return notif 
 end)
