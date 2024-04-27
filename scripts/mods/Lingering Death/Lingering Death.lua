131a1fdf6407488b050dca3ddda72fa1bcb09fd5
Ben <Lai>
2024-04-27 20:26:46 +1000

    Fix for Path patch

--- scripts/mods/Lingering Death/Lingering Death.lua
+++ scripts/mods/Lingering Death/Lingering Death.lua
@@ -1,16 +1,12 @@
 --[[
 Title: Lingering Death
 Author: Wobin
-Date: 23/02/2024
-Version: 1.3
+Date: 27/04/2024
+Version: 1.4
 Repository: https://github.com/Wobin/LingeringDeath
 ]]--
 local mod = get_mod("Lingering Death")
 
-mod:hook(CLASS.HudElementCombatFeed, "_create_notification_entry", function(func, self, notification_type)    
-    local notif = func(self, notification_type)
-    if notif then
-      notif.total_time = mod:get("combat_list_delay") or 5
-    end
-    return notif 
+mod:hook_safe("HudElementCombatFeed", "init", function(self)
+    self._message_duration = mod:get("combat_list_delay") or 5
 end)
